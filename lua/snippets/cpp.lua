e7c569cd69320c0006f0ccb8897dc85956ae7d5b
Pablo Mercado <pablomercado0123@gmail.com>
2025-06-18 13:29:15 -0300

    adds cpp snippets

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -12,9 +12,17 @@ return{
 
             #define forn(i,s,e) for(int i = (s); i<(e); i++)
             #define fori(i,s,e) for(int i = (s); i<=(e); i++)
+            #define forr(i,s,e) for(int i = (s); i>=(e); i--)
             #define clean(v,e) memset(v,e,sizeof(v))
             #define all(x) x.begin(),x.end()
             #define debug(x) cout<< "x: " << (x) << "\n";
+            #define pii pair<int,int>
+            #define vi vector<int>
+            #define vii vector<pair<int,int>
+            #define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+            #define MAXN 200005
+
 
             int main(){{
 
